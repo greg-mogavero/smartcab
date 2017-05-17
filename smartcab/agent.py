61fb7df9c1e7ad5bbe33055c847a5481002f35c2
Greg Mogavero <vgmd93@gmail.com>
2017-05-17 10:02:49 -0700

    Improved the Q-learning algorithm by picking a better epsilon-decay function.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -23,6 +23,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set any additional class parameters as needed
+        self.trial = 0 # keeps track of the number of trials for epsilon decay
 
 
     def reset(self, destination=None, testing=False):
@@ -43,8 +44,8 @@ class LearningAgent(Agent):
             self.epsilon = 0.0
             self.alpha = 0.0
         else:
-            # naive decay function
-            self.epsilon -= 0.05
+            self.epsilon = -((0.01 * self.trial) ** 10) + 1.0
+            self.trial += 1
 
         return None
 
@@ -190,14 +191,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, display=False, log_metrics=True, update_delay=0.01)
+    sim = Simulator(env, display=False, log_metrics=True, update_delay=0.001, optimized=True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(n_test=100)
 
 
 if __name__ == '__main__':
